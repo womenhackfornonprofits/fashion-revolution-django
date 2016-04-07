588897609f19be8270e0bde6fc979612bbacd147
Liliana Kastilio <kastilio.liliana@gmail.com>
2016-04-07 22:18:32 +0100

    Added whitenoise middlewear

--- fashrevwall/settings/__init__.py
+++ fashrevwall/settings/__init__.py
@@ -15,12 +15,14 @@ INSTALLED_APPS = (
 )
 
 MIDDLEWARE_CLASSES = (
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
 )
 
 ROOT_URLCONF = 'fashrevwall.urls'
