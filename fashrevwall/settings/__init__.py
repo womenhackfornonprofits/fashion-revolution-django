2fe7a6edc01abe7672f90c8e297faf06d9c9c666
Liliana Kastilio <kastilio.liliana@gmail.com>
2016-04-07 22:29:41 +0100

    moved static files inside app

--- fashrevwall/settings/__init__.py
+++ fashrevwall/settings/__init__.py
@@ -68,7 +68,7 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "fashrevwall/static"),
 ]
 
 CONTEXT_PROCESSORS = [
