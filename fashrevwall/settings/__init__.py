f860b67e26863e89b853867a1efc3f409eca2069
Liliana Kastilio <kastilio.liliana@gmail.com>
2016-04-07 22:56:52 +0100

    trying to find teh right static path

--- fashrevwall/settings/__init__.py
+++ fashrevwall/settings/__init__.py
@@ -60,7 +60,7 @@ USE_TZ = True  # Stores dates with timezone
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'fashrevwall/staticfiles')
 
 STATIC_URL = '/static/'
 
