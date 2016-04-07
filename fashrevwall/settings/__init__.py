25d3da52ab9796e37a16c2dfc60089910c593fc9
Liliana Kastilio <kastilio.liliana@gmail.com>
2016-04-07 22:03:06 +0100

    Static files aaaaagain

--- fashrevwall/settings/__init__.py
+++ fashrevwall/settings/__init__.py
@@ -58,7 +58,7 @@ USE_TZ = True  # Stores dates with timezone
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
