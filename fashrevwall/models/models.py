0a31433bf91cfdcaf512947a112126b1fae482ef
Raquel Alegre <r.alegre@ucl.ac.uk>
2016-04-08 09:20:16 +0100

    Moved models to better place.

--- fashrevwall/models/models.py
+++ fashrevwall/models/models.py
@@ -3,3 +3,4 @@ from django.db import models
 class Tweet(models.Model):
     text = models.CharField(max_length=200)
     image_url = models.CharField(max_length=200)
+    user = models.CharField(max_length=100)
