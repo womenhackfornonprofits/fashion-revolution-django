7e6f818a256fd5a0737ce6e0a41b6ed41587a426
Raquel Alegre <r.alegre@ucl.ac.uk>
2016-04-08 09:52:08 +0100

    Make model return image_url.

--- fashrevwall/models/models.py
+++ fashrevwall/models/models.py
@@ -3,3 +3,6 @@ from django.db import models
 class Tweet(models.Model):
     image_url = models.CharField(max_length=200, unique=True)
     user = models.CharField(max_length=100)
+
+    def __unicode__(self):
+        return self.image_url
