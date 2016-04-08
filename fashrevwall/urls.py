a5124170eb1315a4de23ab3d586aa7c58d7b3f37
Liliana Kastilio <kastilio.liliana@gmail.com>
2016-04-08 23:34:25 +0100

    Adding in all the views/templates, will create a proper tweet images list view in the next PR

--- fashrevwall/urls.py
+++ fashrevwall/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
 from django.contrib import admin
-from fashrevwall.views import IndexView
+from fashrevwall.views import IndexView, AboutView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     # Index - homepage.
     url(r'^$', IndexView.as_view(), name="index"),
+    url(r'^about/', AboutView.as_view(), name="about"),
 
 ]
