025d452733187302163972a2ae385b164c42f9a6
Raquel Alegre <r.alegre@ucl.ac.uk>
2016-04-09 16:10:51 +0100

    First drafty version of mgmt command using the Twitter client.

--- fashrevwall/twitter_client/management/commands/get_tweets.py
+++ fashrevwall/twitter_client/management/commands/get_tweets.py
@@ -1,11 +1,13 @@
 from django.core.management import BaseCommand
+from fashrevwall.twitter_client.TwitterClient import TwitterClient
 
 #The class must be named Command, and subclass BaseCommand
 class Command(BaseCommand):
 
         # Show this when the user types help
-        help = "Invoke the Twitter client and get some tweets"
+        help = "Invoke the Twitter client and get some tweets."
 
         # A command must define handle()
         def handle(self, *args, **options):
-            self.stdout.write("Gonna get some tweets!")
+            client = TwitterClient()
+            client.get_images_by_hashtag("#spain", 200)
