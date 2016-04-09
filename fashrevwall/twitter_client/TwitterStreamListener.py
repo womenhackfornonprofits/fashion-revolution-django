59b196b1abaafc250916ae8187c76df5f06198ed
Raquel Alegre <r.alegre@ucl.ac.uk>
2016-04-09 10:43:14 +0100

    Trying to save streaming tweets in DB.

--- fashrevwall/twitter_client/TwitterStreamListener.py
+++ fashrevwall/twitter_client/TwitterStreamListener.py
@@ -1,15 +1,23 @@
 import tweepy, sys
+from fashrevwall.wall.models import Tweet
 
 class TwitterStreamListener(tweepy.StreamListener):
 
- def on_status(self, status):
-    print status.author.screen_name.encode('utf-8'), status.created_at, status.text.encode('utf-8')
-    # TODO: Maybe write these to the postgres DB from here?
+    def on_status(self, status):
+        user = status.author.screen_name.encode('utf-8')
+        try:
+            image_url = status.entities['media'][0]['media_url']
+        except KeyError:
+            print "No media in tweet with ID: {}".format(tweet.id)
+            return
+        tweet = Tweet.objects.create(user=user, image_url=image_url)
+        tweet.save()
 
- def on_error(self, status_code):
-    print >> sys.stderr, 'Encountered error with status code:', status_code
-    return True # Don't kill the stream
 
- def on_timeout(self):
-    print >> sys.stderr, 'Timeout...'
-    return True # Don't kill the stream
+    def on_error(self, status_code):
+        print >> sys.stderr, 'Encountered error with status code:', status_code
+        return True # Don't kill the stream
+
+    def on_timeout(self):
+        print >> sys.stderr, 'Timeout...'
+        return True # Don't kill the stream
