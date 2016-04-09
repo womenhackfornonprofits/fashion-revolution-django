f775650f622e6189f3badd9b39c75959a7735a37
Raquel Alegre <r.alegre@ucl.ac.uk>
2016-04-09 09:46:24 +0100

    Get Twitter  secrets from env vars instead of secrets file.

--- fashrevwall/twitter_client/TwitterClient.py
+++ fashrevwall/twitter_client/TwitterClient.py
@@ -2,6 +2,7 @@
 Represents a client that connects to Twitter to retrieve tweets based on a
 search criteria using Twitter REST API and Tweepy.
 """
+import os
 import json
 import tweepy
 from tweepy import OAuthHandler
@@ -13,16 +14,12 @@ class TwitterClient:
 
     def _get_twitter_api(self):
         """
-        Since we are only reading public information from Twitter, we don't need
-        access token/secret values.
+        Get access to Twitter API using dev fashrevwall Twitter account.
         """
-        with open('secrets.json') as secrets_file:
-            secrets = json.load(secrets_file)
-
-        consumer_key = secrets['consumer_key']
-        consumer_secret = secrets['consumer_secret']
-        access_token = secrets['access_token']
-        access_secret = secrets['access_secret']
+        consumer_key = os.environ.get('TWITTER_CONSUMER_KEY')
+        consumer_secret = os.environ.get('TWITTER_CONSUMER_SECRET')
+        access_token = os.environ.get('TWITTER_ACCESS_TOKEN')
+        access_secret = os.environ.get('TWITTER_ACCESS_SECRET')
 
         self.auth = OAuthHandler(consumer_key, consumer_secret)
         self.auth.set_access_token(access_token, access_secret)
