7bacf452966c9a404a8f550e4c337739eef746b8
Raquel Alegre <r.alegre@ucl.ac.uk>
2016-04-11 09:46:00 +0100

    Added created_at fielld in Tweet DB table

--- fashrevwall/wall/models.py
+++ fashrevwall/wall/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.utils import timezone
 
 
 class Tweet(models.Model):
     image_url = models.CharField(max_length=200, unique=True)
     user = models.CharField(max_length=100)
-    
+    created_at = models.DateTimeField(default=timezone.now)
+
     def __unicode__(self):
         return self.image_url
