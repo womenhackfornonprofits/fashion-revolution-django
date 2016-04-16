7fa94ca1e2e158317d452bc1843371c763db67dd
Raquel Alegre <r.alegre@ucl.ac.uk>
2016-04-16 14:30:52 +0100

    Displaying some more log.

--- fashrevwall/twitter_client/TwitterClient.py
+++ fashrevwall/twitter_client/TwitterClient.py
@@ -54,19 +54,20 @@ class TwitterClient:
                 # Some tweets with given hashtag might not have images in them
                 log.info("This tweet doesn't contain an image.")
                 continue
-            log.info("Checking how many tweets are in the DB...")
             num_tweets = Tweet.objects.count()
-            log.info("There are " + str(num_tweets))
+            log.info("There are " + str(num_tweets) + " tweets in the DB.")
             if num_tweets >= 10000:
                 log.info("Maximum number of tweets stored in the DB reached.")
                 oldest_tweet = Tweet.objects.order_by('created_at')[0]
                 log.info("Deleting tweet created on " + str(oldest_tweet.created_at))
                 oldest_tweet.delete()
             try:
+                log.info("Trying to store tweet with text: " + tweet.text + " , and image: " + image_url + " , created at: " + str(tweet.created_at))
                 t = Tweet.objects.create(user=user, image_url=image_url, created_at=created_at)
                 t.save()
                 log.info("New tweet created on date " + str(t.created_at) + " ingested.\n\n")
             except IntegrityError:
+                log.info("Skipping tweet ingestion because image url is already in DB.")
                 # We only want images to be in the DB once so that field has
                 # been set to unique. If we try to insert the same image_url
                 # twice, the code breaks with an IntegrityError, so skip those
