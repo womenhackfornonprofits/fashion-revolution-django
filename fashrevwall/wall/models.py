fee5bd13126fe177302b26fc84df06b2947329b4
Liliana Kastilio <kastilio.liliana@gmail.com>
2016-04-20 07:51:28 +0100

    Added index to created_at to correct order, added the video

--- fashrevwall/wall/models.py
+++ fashrevwall/wall/models.py
@@ -5,10 +5,10 @@ from django.utils import timezone
 class Tweet(models.Model):
     image_url = models.CharField(max_length=200, unique=True)
     user = models.CharField(max_length=100)
-    created_at = models.DateTimeField(default=timezone.now)
+    created_at = models.DateTimeField(db_index=True, default=timezone.now)
     
     class Meta:
-        ordering = ["-id"]
+		ordering = ["-id"]
 
     def __unicode__(self):
         return self.image_url
